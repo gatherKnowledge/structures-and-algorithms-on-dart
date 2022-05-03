44b53d3209ee9e5c94c4aadda4507613487a741d
luke.moon <gathering1143@gmail.com>
2022-05-04 00:33:43 +0900

    add: factorial by loop

--- recursive/factorial.dart
+++ recursive/factorial.dart
@@ -1,13 +1,38 @@
+const int intMaxValue = 9223372036854775807;
 void main() {
-  print(getFactorial(0));
+  int inputInteger = 20;
+  print(getFactorial(inputInteger));
+  print(getFactorialByLoop(inputInteger));
+  // int inputInteger = 21;
+  // print(getFactorial(inputInteger));
+  // print(getFactorialByLoop(inputInteger));
 }
 
 int getFactorial(int integer) {
   if (integer < 0) {
-    return throw  UnimplementedError('Cannot be supplied under zero');
+    return throw UnimplementedError('Cannot be supplied under zero');
   } else if (integer == 0) {
     return 1;
   } else {
-    return integer * getFactorial(integer - 1);
+    int multipled = integer * getFactorial(integer - 1);
+    if (intMaxValue < multipled)
+      throw UnimplementedError('overflow');
+    else if (multipled < 1) throw UnimplementedError('overflow');
+    return multipled;
   }
+}
+
+int getFactorialByLoop(int integer) {
+  int multipled = 1;
+  if (integer == 0)
+    return multipled;
+  else if (integer < 0)
+    throw UnimplementedError('Cannot be supplied under zero');
+  for (var i = integer; i > 0; i--) {
+    multipled *= i;
+    if (intMaxValue < multipled)
+      throw UnimplementedError('overflow');
+    else if (multipled < 1) throw UnimplementedError('overflow');
+  }
+  return multipled;
 }
